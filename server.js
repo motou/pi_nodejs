861c3bf0fa4223a0938ce3e522ca4c1c34a02fb3
Zhen Wang <motou77@gmail.com>
2017-05-24 23:06:34 +0200

    trigger automatic build on docker.io

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 // reply to request with "Hello World!"
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('<h1>Hello World!</h1>');
 });
 
 //start a server on port 80 and log its start to our console
